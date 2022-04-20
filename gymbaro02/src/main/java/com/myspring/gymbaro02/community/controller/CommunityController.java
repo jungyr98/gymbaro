f29ff7cc9ceeb431f62ceee4970994c34b17268d
YuraJung <jungyr98@naver.com>
2022-04-20 15:29:00 +0900

    전체 패키지 생성 완료
    vo, dao, service, controller 패키지 생성 완료

---
+++ gymbaro02/src/main/java/com/myspring/gymbaro02/community/controller/CommunityController.java
@@ -0,0 +1,51 @@
+package com.myspring.gymbaro02.community.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller("communityController")
+@EnableAspectJAutoProxy
+public class CommunityController {
+
+	@RequestMapping(value= "/community/community.do" ,method={RequestMethod.POST,RequestMethod.GET})
+	public ModelAndView community(HttpServletRequest request, HttpServletResponse response) throws Exception{
+		HttpSession session;
+		session = request.getSession();
+		session.setAttribute("section", 1);
+		session.setAttribute("pageNum", 1);
+		ModelAndView mav=new ModelAndView();
+		String viewName=(String)request.getAttribute("viewName");
+		mav.setViewName(viewName);
+
+		return mav;
+	}
+	
+	@RequestMapping(value= "/community/communityDetail.do" ,method={RequestMethod.POST,RequestMethod.GET})
+	public ModelAndView communityDetail(HttpServletRequest request, HttpServletResponse response) throws Exception{
+		HttpSession session;
+		ModelAndView mav=new ModelAndView();
+		String viewName=(String)request.getAttribute("viewName");
+		mav.setViewName(viewName);
+
+		return mav;
+	}
+	
+	@RequestMapping(value= "/community/communityWrite.do" ,method={RequestMethod.POST,RequestMethod.GET})
+	public ModelAndView communityWrite(HttpServletRequest request, HttpServletResponse response) throws Exception{
+		HttpSession session;
+		ModelAndView mav=new ModelAndView();
+		String viewName=(String)request.getAttribute("viewName");
+		mav.setViewName(viewName);
+
+		return mav;
+	}
+
+}
+
---
+++ gymbaro02/src/main/java/com/myspring/gymbaro02/goods/controller/GoodsController.java
@@ -0,0 +1,39 @@
+package com.myspring.gymbaro02.goods.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller("goodsController")
+@EnableAspectJAutoProxy
+public class GoodsController {
+
+	@RequestMapping(value= "/goods/searchGoods.do" ,method={RequestMethod.POST,RequestMethod.GET})
+	public ModelAndView searchGoods(HttpServletRequest request, HttpServletResponse response) throws Exception{
+		HttpSession session;
+		session = request.getSession();
+		session.setAttribute("section", 1);
+		session.setAttribute("pageNum", 1);
+		ModelAndView mav=new ModelAndView();
+		String viewName=(String)request.getAttribute("viewName");
+		mav.setViewName(viewName);
+
+		return mav;
+	}
+	
+	@RequestMapping(value= "/goods/goodsInfo.do" ,method={RequestMethod.POST,RequestMethod.GET})
+	public ModelAndView goodsInfo(HttpServletRequest request, HttpServletResponse response) throws Exception{
+		HttpSession session;
+		ModelAndView mav=new ModelAndView();
+		String viewName=(String)request.getAttribute("viewName");
+		mav.setViewName(viewName);
+
+		return mav;
+	}
+}
